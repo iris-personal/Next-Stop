576077424d5184df5824a38aec9bd5abaf4ffb4f
claudee <c.cabalquinto@gmail.com>
2022-08-14 11:29:14 -0700

    models activity

--- main_app/models.py
+++ main_app/models.py
@@ -18,3 +18,14 @@ class Trip(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'trip_id': self.id})
+
+class Activity(models.Model):
+    d_time = models.DateTimeField('Activity Day')
+    activity = models.CharField(max_length=100)
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.get_activity_display()} on {self.d_time}'
+    
+    class Meta:
+        ordering = ['d_time']
