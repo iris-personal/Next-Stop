0f018be4f09612f2df71090d2c47e109f98c6d0d
lilliesheely <lilliesheely@gmail.com>
2022-08-15 10:36:16 -0700

    Destination View Function & Env

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Trip, Activity
+from .models import Trip, Activity, Destination
 
 # Register your models here.
 
 admin.site.register(Trip)
 admin.site.register(Activity)
+admin.site.register(Destination)
--- main_app/urls.py
+++ main_app/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('trips/<int:trip_id>/add_activity/', views.add_activity, name='add_activity'),
     path('trips/<int:pk>/update_activity/', views.ActivitiesUpdate.as_view(), name='activities_update'),
     path('trips/<int:pk>/delete_activity/', views.ActivitiesDelete.as_view(), name='activities_delete'),
+    path('destinations/', views.destinations, name='destinations'),
+    path('destinations/create/', views.destinations_create, name='destinations_create'),
 ]
