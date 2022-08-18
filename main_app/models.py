ca9880aa8e2df19c87ec9c6e1399f6992d07b376
claudee <c.cabalquinto@gmail.com>
2022-08-18 16:48:52 -0700

    a_time

--- main_app/models.py
+++ main_app/models.py
@@ -31,7 +31,7 @@ class Activity(models.Model):
         return f'{self.activity} on {self.a_date}'
     
     class Meta:
-        ordering = ['a_date']
+        ordering = ['a_date', 'a_time']
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'trip_id': self.trip.id})
